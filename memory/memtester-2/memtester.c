76955a984f6ec695c973eb1d0070f9be74309953
wangwei1 <wangwei1@lixiang.com>
2022-02-21 11:58:10 +0800

    add memtester2

---
+++ memory/memtester-2/memtester.c
@@ -0,0 +1,18 @@
+/*
+ * 
+ * memtester version 1
+ * 
+ */
+
+#include <stdio.h>
+
+#define __version__ "1.0"
+
+int main(int argc, char** argv){
+    printf("argc=%d\n",argc);
+    printf("argv=%s\n",argv[0]);
+    // if(argc){
+
+    // }
+
+}
