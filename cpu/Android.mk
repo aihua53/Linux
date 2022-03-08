88d8c0c010259c4955ac5629901f0e3499c33550
wangwei1 <wangwei1@lixiang.com>
2022-03-08 08:53:09 +0800

    update cputest

---
+++ cpu/Android.mk
@@ -0,0 +1,8 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE_TAGS:= optional
+LOCAL_MODULE := cputest
+LOCAL_SRC_FILES := ./cputest.c
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_EXECUTABLE)
